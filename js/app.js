541bafcb7fd4977c05ebbef1990afd61c2149fcc
MaionePietro <pietromaionespam@gmail.com>
2023-02-12 23:38:45 +0100

    FEAT onClick

--- js/app.js
+++ js/app.js
@@ -7,21 +7,60 @@ const grigliaElement = document.querySelector('.griglia')
 const btnElement = document.querySelector('.btn');
 
 
-btnElement.addEventListener('click', function (){
+btnElement.addEventListener('click', function () {
     preGame.classList.remove('active');
     inGame.classList.add('active');
+    newGame();
+    bombe();
 })
 
-for (let i = 0; i < numeroCelle; i++) {
-    let num = i + 1;
-    let divString = `<div class="cella" style="width: calc(100% / ${latoGriglia});">${num}</div>`;
-    grigliaElement.innerHTML += divString;
+
+
+
+
+function newGame() {
+    for (let i = 0; i < numeroCelle; i++) {
+        let num = i + 1;
+        let divString = `<div class="cella" style="width: calc(100% / ${latoGriglia});">${num}</div>`;
+        grigliaElement.innerHTML += divString;
+    }
+
+    const clickElement = document.querySelectorAll('.cella');
+
+    for (let i = 0; i < clickElement.length; i++) {
+        const cella = clickElement[i]
+        clickElement[i].addEventListener('click', function () {
+            clickElement[i].classList.add('trasparenza');
+        })
+        cella.addEventListener('click', onClick)
+    }
+
 }
 
-const clickElement = document.querySelectorAll('.cella');
+function onClick(event) {
+    console.log(event)
+    console.log(event.target)
+    console.log(this)
+    console.log(event.target === this)
+    // const cella = event.target
+    const cella = this
+    console.log(cella.innerHTML)
 
-for(let i=0 ; i <clickElement.length ; i++){
-    clickElement[i].addEventListener('click', function (){
-        clickElement[i].classList.add('trasparenza');
-    })
+    const quadrato = parseInt(cella.innerHTML) ** 2
+    console.log(quadrato)
+
+    cella.classList.add('bg-green')
+    cella.removeEventListener('click', onClick)
+    // console.log(i + 1)
+}
+
+function bombe() {
+    let arrayBombe = [];
+    while (arrayBombe.length < 16) {
+        let randomNumber = Math.floor(Math.random() * 100) + 1;
+        if (arrayBombe.indexOf(randomNumber) === -1) {
+            arrayBombe.push(randomNumber);
+        }
+    }
+    console.log(arrayBombe);
 }
